3164f5ee4647818d8c3d38fc2727a97f7f9f603b
LeighCressman <crel@pennmedicine.upenn.edu>
2021-07-07 13:13:48 -0400

    daily update

--- phl_vacc_push.sh
+++ phl_vacc_push.sh
@@ -3,7 +3,7 @@ cd ~/phl_covid/SARS-CoV-2-Regional-Metadata
 git pull
 git add .
 git commit -m "daily update"
-
+git push -u origin main
 # use autoexpect tool to create .exp script
 # .exp script stores and enters github credentials
 #./github_push.exp
