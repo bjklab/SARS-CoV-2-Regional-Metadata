ee23a19f47a42bb35dd1cfe5e0fd7e7196368f41
LeighCressman <crel@pennmedicine.upenn.edu>
2021-07-12 21:50:02 +0000

    update

--- phl_vacc_push.sh
+++ phl_vacc_push.sh
@@ -2,5 +2,6 @@
 cd ~/phl_covid/SARS-CoV-2-Regional-Metadata
 git pull
 git add .
+
 git commit -m "daily update"
 git push -u origin main
--- phl_vacc_relative.R
+++ phl_vacc_relative.R
@@ -20,12 +20,17 @@ phl_vacc <- get_carto(query = "SELECT * FROM covid_vaccines_by_zip",
 #######################################
 # export and zip file
 #######################################
+# 
+# write_csv(phl_vacc, paste("./phl_vacc_", Sys.Date(),
+#                           ".csv", sep = ""))
+# files2zip <- list.files(path = "./", pattern = "csv")
+# zip(zipfile = "phl_vacc_daily_data.zip", files = files2zip)
 
-write_csv(phl_vacc, paste("./raw_phl_vacc_data/phl_vacc_", Sys.Date(), 
+write_csv(phl_vacc, paste("./raw_phl_vacc_data/phl_vacc_", Sys.Date(),
                           ".csv", sep = ""))
 
 files2zip <- list.files(path = "./raw_phl_vacc_data", pattern = "csv", full.names = TRUE)
 
 zip(zipfile = "phl_vacc_daily_data.zip", files = files2zip, root = ".",
-    mode = "cherry-pick") 
+    mode = "cherry-pick")
 
