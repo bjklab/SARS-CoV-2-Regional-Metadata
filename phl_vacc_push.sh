a85f01dbf6562330c775bbb1d3a2cacb4de1cad8
LeighCressman <crel@pennmedicine.upenn.edu>
2021-07-13 12:44:23 +0000

    daily update

--- phl_vacc_push.sh
+++ phl_vacc_push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/phl_covid/SARS-CoV-2-Regional-Metadata
-#git pull | tee -a ~/debugging.log
+git pull
 git add .
 git commit -m "daily update"
-git push 2>&1 | tee -a ~/debugging.log
+git push
